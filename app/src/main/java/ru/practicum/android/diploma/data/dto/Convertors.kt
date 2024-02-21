42882401b7aa391b9f5c0c20a292b36bd99be162
Practicum <practicum@ya.ru>
2024-02-21 14:07:57 +0700

    HHit-46: Определила сущность БД для избранных ваканчий

--- app/src/main/java/ru/practicum/android/diploma/data/dto/Convertors.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/Convertors.kt
@@ -8,26 +8,26 @@ import ru.practicum.android.diploma.domain.model.VacanciesModel
 import ru.practicum.android.diploma.domain.model.VacancyModel
 
 class Convertors {
-    fun convertorToDetailVacancy(vacancy: VacancyDetailedDto): DetailVacancy {
+    fun convertorToDetailVacancy(vacancyDto: VacancyDetailedDto): DetailVacancy {
         return DetailVacancy(
-            id = vacancy.id,
-            areaName = vacancy.area?.name,
-            areaUrl = vacancy.employer?.logoUrls?.logoUrl240,
-            contactsEmail = vacancy.contacts?.email,
-            contactsName = vacancy.contacts?.name,
-            contactsPhones = vacancy.contacts?.phones.let { list -> list?.map { createPhone(it) } },
-            comment = null,
-            description = vacancy.description,
-            employerName = vacancy.employer?.name,
-            employmentName = vacancy.employment.name,
-            experienceName = vacancy.experience.name,
-            keySkillsNames = createKeySkills(vacancy.keySkills),
-            name = vacancy.name,
-            salaryCurrency = vacancy.salary?.currency,
-            salaryFrom = vacancy.salary?.from,
+            id = vacancyDto.id,
+            areaName = vacancyDto.area.name,
+            areaUrl = vacancyDto.employer?.logoUrls?.logoUrl240 ?: "",
+            contactsEmail = vacancyDto.contacts?.email ?: "",
+            contactsName = vacancyDto.contacts?.name ?: "",
+            contactsPhones = vacancyDto.contacts?.phones?.let { list -> list?.map { createPhone(it) } }
+                ?: listOf(),
+            description = vacancyDto.description,
+            employerName = vacancyDto.employer?.name ?: "",
+            employmentName = vacancyDto.employment?.name ?: "",
+            experienceName = vacancyDto.experience.name ?: "",
+            keySkillsNames = createKeySkills(vacancyDto.keySkills),
+            name = vacancyDto.name,
+            salaryCurrency = vacancyDto.salary?.currency ?: "",
+            salaryFrom = vacancyDto.salary?.from,
             salaryGross = false,
-            salaryTo = vacancy.salary?.to,
-            scheduleName = vacancy.schedule?.name,
+            salaryTo = vacancyDto.salary?.to,
+            scheduleName = vacancyDto.schedule?.name ?: "",
         )
     }
 
@@ -35,7 +35,7 @@ class Convertors {
         return VacancyModel(
             id = vacancy.id,
             vacancyName = vacancy.name,
-            city = vacancy.area?.name,
+            city = vacancy.area.name,
             salary = "100",
             companyName = null,
             logoUrls = vacancy.employer?.logoUrls?.logoUrl240,
@@ -56,7 +56,8 @@ class Convertors {
         return "+${phone.country}" + " (${phone.city})" + " ${phone.number}"
     }
 
-    private fun createKeySkills(keySkills: List<KeySkillsDto>?): List<String?> {
-        return keySkills?.map { it.name } ?: emptyList()
+    private fun createKeySkills(keySkills: List<KeySkillsDto>): List<String> {
+        //  return keySkills.map { it.name } ?: emptyList()
+        return keySkills.mapNotNull { it.name }.filter { it.isNotEmpty() } ?: emptyList()
     }
 }
