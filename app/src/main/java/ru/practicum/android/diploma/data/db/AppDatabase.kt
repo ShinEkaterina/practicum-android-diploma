483885db90a6e8f095536235f46e2b92ec1f7f16
Practicum <practicum@ya.ru>
2024-02-21 13:19:57 +0700

    HHit-46: Поправила обязательность полей в VacancyDetailedDto

--- app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/AppDatabase.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import ru.practicum.android.diploma.data.db.dao.VacancyDao
 import ru.practicum.android.diploma.data.db.entity.VacancyEntity
 
 @Database(
@@ -10,5 +11,5 @@ import ru.practicum.android.diploma.data.db.entity.VacancyEntity
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-    // abstract fun vacancyDao(): VacancyDao
+     abstract fun vacancyDao(): VacancyDao
 }
--- app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailedDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/dto/VacancyDetailedDto.kt
@@ -12,16 +12,16 @@ import ru.practicum.android.diploma.data.dto.field.ScheduleDto
 
 data class VacancyDetailedDto(
     val id: String,
-    val area: AreaDto?,
+    val name: String,
+    val area: AreaDto,
     val contacts: ContactsDto?,
-    val description: String?,
+    val description: String,
     val employer: EmployerDto?,
-    val employment: EmploymentDto,
+    val employment: EmploymentDto?,
     val experienceId: String?,
     val experience: ExperienceDto,
     @SerializedName("key_skills")
-    val keySkills: List<KeySkillsDto>?,
-    val name: String,
+    val keySkills: List<KeySkillsDto> = listOf(),
     val salary: SalaryDto?,
     val schedule: ScheduleDto?,
 )
