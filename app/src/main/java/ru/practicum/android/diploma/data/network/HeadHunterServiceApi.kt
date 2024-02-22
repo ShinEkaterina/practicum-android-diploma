0e6d39b204789adb19bf8c119e3064ea18b14553
Sergun <ya.tarannov@yandex.ru>
2024-02-22 21:26:58 +0300

    Добавил токен

--- app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterServiceApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HeadHunterServiceApi.kt
@@ -1,8 +1,10 @@
 package ru.practicum.android.diploma.data.network
 
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.data.dto.respone.SearchResponse
 import ru.practicum.android.diploma.data.dto.respone.VacanciesSearchResponse
 import ru.practicum.android.diploma.data.dto.respone.VacancyDetailedResponse
@@ -22,7 +24,9 @@ interface HeadHunterServiceApi {
         @Query("page") page: Long,
         @Query("per_page") amount: Long
     ): VacanciesSearchResponse
-
+    @Headers(
+        "Authorization: Bearer ${BuildConfig.HH_ACCESS_TOKEN}"
+    )
     @GET("/vacancies/{vacancy_id}")
     suspend fun searchConcreteVacancy(
         @Path("vacancy_id") vacancyId: String
