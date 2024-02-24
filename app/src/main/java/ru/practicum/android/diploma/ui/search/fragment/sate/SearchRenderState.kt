59fe5c74e47e9c06ed04dd282079249e00794597
NamerPRO <mail@namerpro.me>
2024-02-24 17:24:02 +0300

    убрал лишние импорты

--- app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/sate/SearchRenderState.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/search/fragment/sate/SearchRenderState.kt
@@ -1,7 +1,5 @@
 package ru.practicum.android.diploma.ui.search.fragment.sate
 
-import ru.practicum.android.diploma.domain.model.VacanciesModel
-
 sealed interface SearchRenderState {
 
     data object Placeholder : SearchRenderState
