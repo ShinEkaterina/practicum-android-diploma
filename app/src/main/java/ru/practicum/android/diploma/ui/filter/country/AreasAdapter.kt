13e96a381b4989a5684eaa0b120a01ce8d0bbfc5
TOxaREY <delta@toxarey.xyz>
2024-03-01 14:17:39 +0300

    исправления detektFormat 2

--- app/src/main/java/ru/practicum/android/diploma/ui/filter/country/AreasAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/filter/country/AreasAdapter.kt
@@ -6,7 +6,10 @@ import androidx.recyclerview.widget.RecyclerView
 import ru.practicum.android.diploma.databinding.CountryItemBinding
 import ru.practicum.android.diploma.domain.model.AreasModel
 
-class AreasAdapter(private val data: ArrayList<AreasModel>, private val onClick: (AreasModel) -> Unit) : RecyclerView.Adapter<AreasViewHolder> () {
+class AreasAdapter(
+    private val data: ArrayList<AreasModel>,
+    private val onClick: (AreasModel) -> Unit
+) : RecyclerView.Adapter<AreasViewHolder> () {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AreasViewHolder {
         val view = CountryItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return AreasViewHolder(view)
